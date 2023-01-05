5fa0367d952dceda9266385678511f1ce5c0c48f
PaulGillon <pau.gillon0@gmail.com>
2023-01-05 14:17:08 +0100

    tkt

--- src/Controller/SubscriptionCartController.php
+++ src/Controller/SubscriptionCartController.php
@@ -5,20 +5,20 @@ namespace AMAP\Controller;
 
 require 'vendor/autoload.php';
 
-use AMAP\Model\SubsciptionCartModel;
+use AMAP\Model\SubscriptionCartModel;
 
 class SubsciptionCartController
 {
     public function get()
     {
-        $data = (new SubsciptionCartModel)->getAll();
+        $data = (new SubscriptionCartModel)->getAll();
         $response = ['status' => 200, 'commentaires' => $data];
         echo json_encode($response);
     }
 
     public function insert($data)
     {
-        if ((new SubsciptionCartModel)->insert($data)) {
+        if ((new SubscriptionCartModel)->insert($data)) {
             $response = ['status' => 200, 'message' => "Le commentaire a bien été enregistrée"];
         } 
         else {
