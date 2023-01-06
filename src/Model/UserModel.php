27f9541edb3bac4179c4884cf0b7dc0ca6a71986
PaulGillon <pau.gillon0@gmail.com>
2023-01-06 14:40:48 +0100

    tkt

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -49,14 +49,26 @@ class UserModel
         // Sinon
         // return $stmt->execute([...]) sera égale à true
 
-        $request = "INSERT INTO Utilisateur (email, nom, prenom, avatar, regimeAlimentaire, password) VALUES (:email, :nom, :prenom, :avatar, :regimeAlimentaire, :password)";
-        $stmt = $this->pdo->prepare($request);
-        $stmt->bindParam(':email', $data->email, PDO::PARAM_STR);
-        $stmt->bindParam(':nom', $data->nom, PDO::PARAM_STR);
-        $stmt->bindParam(":prenom", $data->prenom, PDO::PARAM_STR);
-        $stmt->bindParam(":avatar", $data->avatar, PDO::PARAM_STR);
-        $stmt->bindParam(":regimeAlimentaire", $data->regimeAlimentaire, PDO::PARAM_STR);
-        $stmt->bindParam(":password", $data->password, PDO::PARAM_STR);
-        return $stmt->execute([$data->id_email, $data->nom, $data->prenom, $data->avatar, $data->regimeAlimentaire, $data->password]);
+        $email = $data->email;
+        $stmt = $this->pdo->prepare("SELECT * FROM Utilisateur WHERE email=?");
+        $stmt->execute([$email]); 
+        $user = $stmt->fetch();
+        if ($user) 
+        {
+            return false;
+        } 
+        else 
+        {
+            $request = "INSERT INTO Utilisateur (email, nom, prenom, avatar, regimeAlimentaire, password) VALUES (:email, :nom, :prenom, :avatar, :regimeAlimentaire, :password)";
+            $stmt = $this->pdo->prepare($request);
+            $stmt->bindParam(':email', $data->email, PDO::PARAM_STR);
+            $stmt->bindParam(':nom', $data->nom, PDO::PARAM_STR);
+            $stmt->bindParam(":prenom", $data->prenom, PDO::PARAM_STR);
+            $stmt->bindParam(":avatar", $data->avatar, PDO::PARAM_STR);
+            $stmt->bindParam(":regimeAlimentaire", $data->regimeAlimentaire, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $data->password, PDO::PARAM_STR);
+            $stmt->execute();   
+            return true;
+        }
     }
 }
